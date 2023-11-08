1bfc3aad781367233e626c347d59df575a378d84
苏忠辉 <szh@cirscn.com>
2023-11-08 23:38:38 +0800

    更新

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,10 +1,29 @@
 import { View } from "@tarojs/components";
 import FooterTabBar from "@/components/FooterTabBar";
-
-import "./index.scss";
+import { Button, Cell } from "@nutui/nutui-react-taro";
+import { Right } from "@nutui/icons-react-taro";
+import style from "./index.module.scss";
 
 function Index() {
-  return <View>{/* <FooterTabBar /> */}</View>;
+  return (
+    <View className={style.container}>
+      <View className={style.header}></View>
+      <View className={style.content}>
+        <Cell.Group className={style.list} divider>
+          <Cell title="申请记录" align="center" extra={<Right />} />
+          <Cell title="用款记录" extra={<Right />} align="center" />
+        </Cell.Group>
+        <Button
+          className={style.button}
+          shape="round"
+          type="default"
+          size="large"
+        >
+          退出登录
+        </Button>
+      </View>
+    </View>
+  );
 }
 
 export default Index;
