c2b2721696d3bf62487ce464998bc6d81147b411
苏忠辉 <szh@cirscn.com>
2023-11-09 15:30:39 +0800

    更新

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,18 +1,32 @@
-import { View } from "@tarojs/components";
+import { View, Text } from "@tarojs/components";
 import { Button, Cell } from "@nutui/nutui-react-taro";
 import { Right } from "@nutui/icons-react-taro";
 import style from "./index.module.scss";
 import Taro from "@tarojs/taro";
-
+import { Avatar } from "@nutui/nutui-react-taro";
+import avatarUrl from "@/assets/avatar.png";
 function Index() {
   const handleClick = (key) => {
     Taro.navigateTo({
       url: `/packages/${key}/index`,
     });
   };
+  const userInfo = {
+    phone: "161****0191",
+    avatarUrl: avatarUrl,
+  };
   return (
     <View className={style.container}>
-      <View className={style.header}></View>
+      <View className={style.header}>
+        <View className={style.info}>
+          <Avatar
+            size="large"
+            className={style.avatar}
+            src={userInfo.avatarUrl}
+          />
+          <Text className={style.name}>{userInfo.phone}</Text>
+        </View>
+      </View>
       <View className={style.content}>
         <Cell.Group className={style.list} divider>
           <Cell
